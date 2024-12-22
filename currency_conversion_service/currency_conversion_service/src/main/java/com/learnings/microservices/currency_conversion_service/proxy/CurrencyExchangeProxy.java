bf9070f39e35341349e10a7c62b6ade2feaf398f
DummuMounika <mounikadummu.edu2024@gmail.com>
2024-12-21 21:47:41 -0600

    first commit for currency conversion service

---
+++ currency_conversion_service/currency_conversion_service/src/main/java/com/learnings/microservices/currency_conversion_service/proxy/CurrencyExchangeProxy.java
@@ -0,0 +1,18 @@
+package com.learnings.microservices.currency_conversion_service.proxy;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.learnings.microservices.currency_conversion_service.bean.CurrencyConversion;
+
+//@FeignClient(name="currency-exchange", url="localhost:8000")
+@FeignClient(name="currency-exchange")
+public interface CurrencyExchangeProxy {
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversion retrieveExchangeValue(
+			@PathVariable String from,
+			@PathVariable String to);	
+
+}
